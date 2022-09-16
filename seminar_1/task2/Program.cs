7c7d8d4929f791c168ddd1ddd7e3183bc44af7d5
krajoff <t0k@bk.ru>
2022-09-16 16:16:00 +0300

    Modify task2 and task4

--- seminar_1/task2/Program.cs
+++ seminar_1/task2/Program.cs
@@ -5,8 +5,8 @@ Console.Write("Input second number: ");
 int numberB = int.Parse(Console.ReadLine());
 
 if (numberA > numberB) 
-    Console.WriteLine($"max = {numberA}, that is {numberA}>{numberB}");
+    Console.WriteLine($"max = {numberA}");
 else if (numberA < numberB)   
-    Console.WriteLine($"max = {numberB}, that is {numberA}<{numberB}");
+    Console.WriteLine($"max = {numberB}");
 else 
     Console.WriteLine($"That is {numberA}={numberB}");
--- seminar_1/task4/Program.cs
+++ seminar_1/task4/Program.cs
@@ -3,3 +3,14 @@ Console.Write("Input first number: ");
 int numberA = int.Parse(Console.ReadLine());
 Console.Write("Input second number: ");
 int numberB = int.Parse(Console.ReadLine());
+Console.Write("Input third number: ");
+int numberC = int.Parse(Console.ReadLine());
+
+if (numberA>numberB && numberA>numberC)
+    Console.WriteLine($"max = {numberA}");
+else if (numberB>numberA && numberB>numberC)
+    Console.WriteLine($"max = {numberB}");
+else if (numberC>numberA && numberC>numberA)
+    Console.WriteLine($"max = {numberC}");
+else
+    return;
