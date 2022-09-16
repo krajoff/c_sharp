49c24b1899669e7648c2ff34041dedc3e9adfb6a
krajoff <t0k@bk.ru>
2022-09-16 14:15:31 +0300

    Add task1(check square) and task3(days)

---
+++ seminar_1/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Input first number: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA == Convert.ToInt32(Math.Pow(numberB, 2))) {
+    Console.WriteLine($"{numberA} is square of {numberB}");
+    }
+else {
+    Console.WriteLine($"{numberA} is not square of {numberB}");
+}
+
+
---
+++ seminar_1/task3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+Console.WriteLine("Input number from 1 to 7");
+int day = int.Parse(Console.ReadLine());
+if (day == 1){
+    Console.WriteLine("Today is monday");
+}
+else if (day == 2){
+    Console.WriteLine("Today is tuesday");
+}
+else{
+    if (day == 3)
+    {
+        Console.WriteLine("Today is wednesday");
+    }
+    else if (day == 4)
+    {
+        Console.WriteLine("Today is thursday");
+    }
+    else if (day == 5)
+    {
+        Console.WriteLine("Today is friday");
+    }
+    else if (day == 6)
+    {
+        Console.WriteLine("Today is suturday");
+    }
+    else if (day == 7)
+    {
+        Console.WriteLine("Today is sunday");
+    }
+    else 
+    {
+        Console.WriteLine("It is out of range");
+    }
+}
