f20bdd7052fcb3739f68e68703998c6dd0c68126
krajoff <t0k@bk.ru>
2022-09-16 16:35:16 +0300

    Add task6 and task8

---
+++ seminar_1/task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Input number: ");
+int n=int.Parse(Console.ReadLine());
+if (n%2 == 0)
+    Console.Write("Да, число четное");
+else
+    Console.Write("Нет, число нечетное");
---
+++ seminar_1/task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Input number: ");
+int n = int.Parse(Console.ReadLine());
+if (n < 0) 
+    Console.Write("Incorrect number");
+else {
+    for (int i = 2; i <= n; i = i + 2)
+        Console.Write(i+" ");
+}
