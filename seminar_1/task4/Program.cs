50bfeb34e17bc54071bda04523bbdefa541161e0
krajoff <t0k@bk.ru>
2022-09-16 14:52:25 +0300

    Add task4

---
+++ seminar_1/task4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Input first number: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int numberB = int.Parse(Console.ReadLine());
