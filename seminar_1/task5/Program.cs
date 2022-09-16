731ff44eeaa896fc09a49f1eb087732c5289895a
krajoff <t0k@bk.ru>
2022-09-16 14:51:37 +0300

    Add task4

---
+++ seminar_1/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Input number: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Result: ");
+for (int i = -n; i < n; i++)
+{
+    Console.Write(i+",");
+} 
+Console.Write(n);
