979a1decdedfd5e104c96ae022c15f4afb36d6d5
krajoff <t0k@bk.ru>
2022-09-16 15:17:27 +0300

    Resolve task7

--- seminar_1/task7/Program.cs
+++ seminar_1/task7/Program.cs
@@ -3,4 +3,5 @@ Console.Write("Input number: ");
 int n = int.Parse(Console.ReadLine());
 Console.Write("Result: ");  
 int a = n/10, b = n - a*10;
-Console.Write(b);  
+int div = n%10;
+Console.Write(div);  
