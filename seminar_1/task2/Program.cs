c50d07d7fa1bae4f3b58cbdfb86078a29c576f2b
krajoff <t0k@bk.ru>
2022-09-16 14:38:01 +0300

    Add task2

---
+++ seminar_1/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Input first number: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB) 
+    Console.WriteLine($"max = {numberA}, that is {numberA}>{numberB}");
+else if (numberA < numberB)   
+    Console.WriteLine($"max = {numberB}, that is {numberA}<{numberB}");
+else 
+    Console.WriteLine($"That is {numberA}={numberB}");
