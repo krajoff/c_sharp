eadb4a0fc5349c8f0056858db1e126cf10d6cff4
krajoff <t0k@bk.ru>
2022-09-16 15:19:17 +0300

    Add check statment task7

--- seminar_1/task7/Program.cs
+++ seminar_1/task7/Program.cs
@@ -1,6 +1,10 @@
 ﻿Console.Clear();
 Console.Write("Input number: ");
 int n = int.Parse(Console.ReadLine());
+if(n<100 || n>999) {
+    Console.WriteLine("Incorrent number");
+    return;
+}
 Console.Write("Result: ");  
 int a = n/10, b = n - a*10;
 int div = n%10;
