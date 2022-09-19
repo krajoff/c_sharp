32f408d94c71a6b650d3b5635d2cb12b37f36987
krajoff <iioz@ya.ru>
2022-09-19 22:58:53 +0300

    Solve task 10

---
+++ seminar_1/task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Input triple digits, please: ");
+int n = int.Parse(Console.ReadLine());
+if(n<100 || n>999) {
+    Console.WriteLine("Incorrent number");
+    return;
+}
+Console.Write("Result: ");
+int SecondDigital = n%100;
+SecondDigital = (SecondDigital - SecondDigital%10)/10;
+Console.Write(SecondDigital);
