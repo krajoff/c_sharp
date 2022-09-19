136e9078a31de7957c8624dfec0b382f1b5222ca
krajoff <iioz@ya.ru>
2022-09-19 23:49:41 +0300

    Solve task 15

---
+++ seminar_1/task15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Input number from 1 to 7: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7){
+    Console.WriteLine("Да, сегодня выходной");
+}
+else {
+    Console.WriteLine("Нет, сегодня будни");
+}
