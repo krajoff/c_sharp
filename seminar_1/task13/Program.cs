f40c8f74e88e8274c854b5771881959292ba469d
krajoff <iioz@ya.ru>
2022-09-19 23:50:44 +0300

    Add task 13

---
+++ seminar_1/task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Input number, please: ");
+int n = int.Parse(Console.ReadLine());
+if(n<100) {
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+int ThirdDigital = n;
+if(n<1000) ThirdDigital = n%10;
+//if(n<10000) ThirdDigital = (n - n%1000)/100;
+//int ThirdDigital = n%1000;
+//ThirdDigital = (ThirdDigital - ThirdDigital%100)/100;
+Console.Write("Result: " + ThirdDigital);
