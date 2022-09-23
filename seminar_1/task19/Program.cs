f584c17bb561c357b43789b1862fb168463534f2
krajoff <iioz@ya.ru>
2022-09-23 22:17:08 +0300

    Solve task19

---
+++ seminar_1/task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число, пожалуйста: ");
+int n = int.Parse(Console.ReadLine());
+if(n<10000 || n>99999) {
+    Console.WriteLine("Некорректное число");
+    return;
+}
+
+String str = n.ToString();
+Console.Write($"{n} - ");
+if(str[0]==str[4]&&str[1]==str[3]) Console.Write("да, палиндром");
+else Console.Write("нет, не палиндром");
