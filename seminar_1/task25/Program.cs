ae237a14b0ad435799367190a72278bbfaf466e0
krajoff <iioz@ya.ru>
2022-09-27 15:18:37 +0300

    Solve task25

---
+++ seminar_1/task25/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: "); 
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: "); 
+int B = int.Parse(Console.ReadLine());
+Console.Write($"Результат: {Math.Pow(A,B)}");
