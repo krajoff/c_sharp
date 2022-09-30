e4bfe710cad685c62c3e98fd62ed8d64070115b9
krajoff <iioz@ya.ru>
2022-09-30 22:26:48 +0300

    Resolve task29 and solve task34

--- seminar_1/task29/Program.cs
+++ seminar_1/task29/Program.cs
@@ -4,14 +4,11 @@ Console.Clear();
 Console.Write("Введите 8 чисел через запятую: ");
 string str = Console.ReadLine();
 int[] array = ReturnArray(str);
-
+Console.Write($"Результат: [{String.Join(", ", array)}]");
 
 int[] ReturnArray(string str)
 {
     int[] nums = str.Split(",").Select(int.Parse).ToArray();
-    int n = nums.Length;
-    Console.Write("Результат: [");
-    for(int i=0; i<n-1; i++) Console.Write(nums[i] + ", ");
-    Console.Write(nums[n-1] + "]");
+    int n = nums.Length;    
     return nums;
 }
---
+++ seminar_1/task34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет 
+// количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] array = GetRandomArray(size);
+Console.WriteLine($"Массив: [{String.Join(", ", array)}]");
+Console.WriteLine($"Количество четных чисел в массиве: {GetEvenNumber(array, size)}");
+
+int[] GetRandomArray(int size) {
+    int[] array = new int [size];
+    for(int i=0; i<size; i++) array[i] = new Random().Next(100,1000);
+    return array;
+}
+
+int GetEvenNumber(int[] array, int size) {
+    int evennumber = 0;
+    for(int i=0; i<size; i++)
+        evennumber += array[i]%2==0 ? 1 : 0;
+    return evennumber;
+}
